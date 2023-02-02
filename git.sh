6b60296c123bae548b4663bcfd0db39684409238
root <root@ip-172-31-39-17.ap-south-1.compute.internal>
2023-02-02 06:04:03 +0000

    push

---
+++ git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git add .
+git commit -m "push"
+git checkout -b nithin
+git push oringin nithin
+
---
+++ odd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {101..999..2}    # start with 1 and increment by 3 until you reach/pass 20
+do
+    echo $i
+done
